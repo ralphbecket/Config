0fea91203cef7ae492abe5776a3559fee8ea4917
Ralph Becket <ralphbecket@outlook.com>
2019-03-08 14:52:06 +1100

    Fri 08/03/2019 14:52:06.90

---
+++ GitSync.bat
@@ -0,0 +1,9 @@
+:: Sync with the repository
+git pull
+:: Add any new files etc.
+git add --all
+:: Show a list of what's new.
+git status
+:: Commit and push the changes to the repository.
+git commit -m "%date% %time%"
+git push
